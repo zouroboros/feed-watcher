9cab1257ff86db66680aa55dc655412ee728656f
Zouroboros <zouroboros@zoho.eu>
2018-08-29 10:39:10 +0200

    Added parceable implementations for Feed and Result.

--- app/src/main/java/me/murks/podcastwatcher/model/Result.kt
+++ app/src/main/java/me/murks/podcastwatcher/model/Result.kt
@@ -1,5 +1,7 @@
 package me.murks.podcastwatcher.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.net.URL
 import java.util.*
 
@@ -7,4 +9,44 @@ import java.util.*
  * @author zouroboros
  */
 data class Result(val feed: Feed, val query: Query, val name: String, val description: String,
-                  val found: Date, val link: URL?, val feedName: String)
+                  val found: Date, val link: URL?, val feedName: String) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readParcelable(Feed::class.java.classLoader),
+            parcel.readParcelable(Query::class.java.classLoader),
+            parcel.readString(),
+            parcel.readString(),
+            Date(parcel.readLong()),
+            if (parcel.readByte() > 0) URL(parcel.readString()) else null,
+            parcel.readString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(feed, flags)
+        parcel.writeParcelable(query, flags)
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeLong(found.time)
+        if(link != null) {
+            parcel.writeByte(1)
+            parcel.writeString(link.toString())
+        } else {
+            parcel.writeByte(0)
+        }
+        parcel.writeString(feedName)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Result> {
+        override fun createFromParcel(parcel: Parcel): Result {
+            return Result(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Result?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
