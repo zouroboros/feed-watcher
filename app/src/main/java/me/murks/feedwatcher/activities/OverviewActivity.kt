d5856641ba6b8666200ab05ef91db692cdfb1d88
Zouroboros <zouroboros@zoho.eu>
2018-09-23 23:41:24 +0200

    OverviewActivity restores correct fragment after screen oriantation change.

--- app/src/main/java/me/murks/feedwatcher/activities/OverviewActivity.kt
+++ app/src/main/java/me/murks/feedwatcher/activities/OverviewActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
@@ -19,6 +20,7 @@ class OverviewActivity : FeedWatcherBaseActivity(), QueriesFragment.OnListFragme
     ResultsFragment.OnListFragmentInteractionListener {
 
     private lateinit var mDrawerLayout: DrawerLayout
+    private var currentFragment = R.id.nav_queries
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,34 +39,7 @@ class OverviewActivity : FeedWatcherBaseActivity(), QueriesFragment.OnListFragme
         navigationView.setNavigationItemSelectedListener { menuItem ->
             mDrawerLayout.closeDrawers()
 
-            when(menuItem.itemId) {
-                R.id.nav_feeds -> {
-                    val transaction = supportFragmentManager.beginTransaction()
-                    transaction.replace(R.id.overview_fragment_container, FeedsFragment())
-                            .addToBackStack(null)
-                    transaction.commit()
-                }
-                R.id.nav_queries -> {
-                    val transaction = supportFragmentManager.beginTransaction()
-                    transaction.replace(R.id.overview_fragment_container, QueriesFragment())
-                            .addToBackStack(null)
-                    transaction.commit()
-                }
-                R.id.nav_add_query -> {
-                    val intent = Intent(this, QueryActivity::class.java)
-                    startActivityForResult(intent, NEW_QUERY_REQUEST)
-                }
-                R.id.nav_add_feed -> {
-                    val intent = Intent(this, FeedActivity::class.java)
-                    startActivity(intent)
-                }
-                R.id.nav_results -> {
-                    val transaction = supportFragmentManager.beginTransaction()
-                    transaction.replace(R.id.overview_fragment_container, ResultsFragment())
-                            .addToBackStack(null)
-                    transaction.commit()
-                }
-            }
+            openFragment(menuItem.itemId)
 
             true
         }
@@ -72,9 +47,11 @@ class OverviewActivity : FeedWatcherBaseActivity(), QueriesFragment.OnListFragme
 
         mDrawerLayout = findViewById(R.id.drawer_layout)
 
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.overview_fragment_container, QueriesFragment())
-        transaction.commit()
+        if (savedInstanceState != null) {
+            openFragment(savedInstanceState.getInt(CURRENT_FRAGMENT))
+        } else {
+            openFragment(currentFragment)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -95,6 +72,7 @@ class OverviewActivity : FeedWatcherBaseActivity(), QueriesFragment.OnListFragme
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        // TODO move this code into the respective activities
         if(requestCode == EDIT_QUERY_REQUEST && resultCode == Activity.RESULT_OK) {
             val query = data!!.getParcelableExtra<Query>(QueryActivity.INTENT_QUERY_EXTRA)
             app.updateQuery(query)
@@ -111,8 +89,46 @@ class OverviewActivity : FeedWatcherBaseActivity(), QueriesFragment.OnListFragme
         startActivity(intent)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(CURRENT_FRAGMENT, currentFragment)
+    }
+
+    private fun openFragment(navId: Int) {
+        currentFragment = navId
+        when(navId) {
+            R.id.nav_feeds -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.overview_fragment_container, FeedsFragment())
+                        .addToBackStack(null)
+                transaction.commit()
+            }
+            R.id.nav_queries -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.overview_fragment_container, QueriesFragment())
+                        .addToBackStack(null)
+                transaction.commit()
+            }
+            R.id.nav_add_query -> {
+                val intent = Intent(this, QueryActivity::class.java)
+                startActivityForResult(intent, NEW_QUERY_REQUEST)
+            }
+            R.id.nav_add_feed -> {
+                val intent = Intent(this, FeedActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_results -> {
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.overview_fragment_container, ResultsFragment())
+                        .addToBackStack(null)
+                transaction.commit()
+            }
+        }
+    }
+
     companion object {
         const val NEW_QUERY_REQUEST = 22
         const val EDIT_QUERY_REQUEST = 101
+        const val CURRENT_FRAGMENT = "current_fragment"
     }
 }
