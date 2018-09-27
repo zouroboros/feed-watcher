cf3e5a827f2b74b71f9ed7a25cdff4b6737ba884
Zouroboros <zouroboros@zoho.eu>
2018-09-27 21:56:36 +0200

    New queries can only be created with at least one filter.

--- app/src/main/java/me/murks/feedwatcher/activities/QueryActivity.kt
+++ app/src/main/java/me/murks/feedwatcher/activities/QueryActivity.kt
@@ -1,22 +1,16 @@
 package me.murks.feedwatcher.activities
 
-import android.app.Activity
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.Button
 import android.widget.EditText
-import me.murks.feedwatcher.FeedWatcherApp
 
 import me.murks.feedwatcher.R
 import me.murks.feedwatcher.model.Filter
 import me.murks.feedwatcher.model.FilterModels
 import me.murks.feedwatcher.model.FilterType
 import me.murks.feedwatcher.model.Query
-
-// TODO only allow the creation of queries with at least one filter
 class QueryActivity : FeedWatcherBaseActivity() {
 
     private lateinit var queryNameText: EditText
@@ -46,9 +40,17 @@ class QueryActivity : FeedWatcherBaseActivity() {
 
         filterList.adapter = filterAdapter
 
-        addFilterButton.setOnClickListener { filterAdapter.filter.add(Filter(FilterType.CONTAINS,
+        addFilterButton.setOnClickListener {
+            filterAdapter.filter.add(Filter(FilterType.CONTAINS,
                 FilterModels.defaultParameter(FilterType.CONTAINS), filterAdapter.itemCount))
             filterList.adapter.notifyItemInserted(filterList.adapter.itemCount - 1)
+            if(filterList.adapter.itemCount > 0 && !saveQueryButton.isEnabled) {
+                saveQueryButton.isEnabled = true
+            }
+        }
+
+        if(filterList.adapter.itemCount == 0) {
+            saveQueryButton.isEnabled = false
         }
 
         saveQueryButton.setOnClickListener {
