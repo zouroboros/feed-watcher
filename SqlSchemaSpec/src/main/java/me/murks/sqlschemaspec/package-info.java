11cd0868707c472cd33ce438dab14016c6a0c101
Zouroboros <zouroboros@zoho.eu>
2019-05-05 15:18:00 +0200

    Removed SqlSchemaSpec project from repository.

--- SqlSchemaSpec/src/main/java/me/murks/sqlschemaspec/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Library for working with sql schemata.
- *
- * Schemata can be created by either defining them directly using the spec
- * classes in this package or by using (anonymous) classes and the
- * {@link me.murks.sqlschemaspec.templates templates} subpackage.
- */
-package me.murks.sqlschemaspec;
--- SqlSchemaSpec/src/main/java/me/murks/sqlschemaspec/templates/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package that contains classes for building sql schemata from
- * (anonymous) classes.
- */
-package me.murks.sqlschemaspec.templates;
