bada1f131d70e5754bee541d521bc3b80b97d01d
Zouroboros <zouroboros@zoho.eu>
2021-05-12 20:04:28 +0200

    Removed unnessary todo.

--- app/src/main/java/me/murks/feedwatcher/AndroidApplication.kt
+++ app/src/main/java/me/murks/feedwatcher/AndroidApplication.kt
@@ -1,10 +1,23 @@
+/*
+This file is part of FeedWatcher.
+
+FeedWatcher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FeedWatcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FeedWatcher. If not, see <https://www.gnu.org/licenses/>.
+Copyright 2021 Zouroboros
+ */
 package me.murks.feedwatcher
 
 import android.app.Application
-import android.app.NotificationManager
-import android.app.NotificationChannel
-import android.os.Build
-
 
 /**
  * @author zouroboros
@@ -16,8 +29,6 @@ class AndroidApplication(): Application() {
         Notifications(this).createNotificationChannel()
 
         val app = FeedWatcherApp(AndroidEnvironment(this))
-        // TODO only schedule job when at least one query is set up
         app.rescheduleJobs()
-
     }
 }
