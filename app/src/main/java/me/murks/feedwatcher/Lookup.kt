22a01957f37ff0b09f88e7924735dae72793dfb1
Zouroboros <zouroboros@zoho.eu>
2021-05-25 20:53:55 +0200

    Made Lookup iterable.

--- app/src/main/java/me/murks/feedwatcher/Lookup.kt
+++ app/src/main/java/me/murks/feedwatcher/Lookup.kt
@@ -1,12 +1,31 @@
+/*
+This file is part of FeedWatcher.
+
+FeedWatcher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FeedWatcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FeedWatcher. If not, see <https://www.gnu.org/licenses/>.
+Copyright 2021 Zouroboros
+ */
 package me.murks.feedwatcher
 
 import java.util.*
 import kotlin.collections.HashMap
 
 /**
+ * Associates a key with a collection of values.
+ *
  * @author zouroboros
  */
-class Lookup<K, V>(private val map: MutableMap<K, MutableList<V>>) {
+class Lookup<K, V>(private val map: MutableMap<K, MutableList<V>>): Iterable<Map.Entry<K, Collection<V>>> {
     fun append(key: K, value: V) {
         if(!map.containsKey(key)) {
             map.put(key, LinkedList())
@@ -18,6 +37,8 @@ class Lookup<K, V>(private val map: MutableMap<K, MutableList<V>>) {
 
     fun flatList(): Collection<Map.Entry<K, V>> = map.entries.map {
         it.value.map { value -> AbstractMap.SimpleEntry(it.key, value) } }.flatten()
+
+    override fun iterator(): Iterator<Map.Entry<K, Collection<V>>> = map.asIterable().iterator()
 }
 
 fun <K, V>Map<K, Collection<V>>.toLookup(): Lookup<K, V> {
